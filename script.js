43aa6d96a880c68e7f4e10017d37cf393da3b9e1
Sanjeevi03 <62064977+Sanjeevi03@users.noreply.github.com>
2021-12-21 13:59:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function show(){
+    document.querySelector(".home").style.cssText="display:block;display:flex;";
+    document.querySelector(".about").style.cssText="display:none;";
+    document.querySelector(".videos").style.cssText="display:none;";
+    document.querySelector(".photo").style.cssText="display:none;";
+    document.querySelector(".more").style.cssText="display:none;";
+}
+function show1(){
+    document.querySelector(".home").style.cssText="display:none;";
+    document.querySelector(".about").style.cssText="display:block;";
+    document.querySelector(".videos").style.cssText="display:none;";
+    document.querySelector(".photo").style.cssText="display:none;";
+    document.querySelector(".more").style.cssText="display:none;";
+}
+function show2(){
+    document.querySelector(".home").style.cssText="display:none;";
+    document.querySelector(".about").style.cssText="display:none;";
+    document.querySelector(".videos").style.cssText="display:block;";
+    document.querySelector(".photo").style.cssText="display:none;";
+    document.querySelector(".more").style.cssText="display:none;";
+}
+function show3(){
+    document.querySelector(".home").style.cssText="display:none;";
+    document.querySelector(".about").style.cssText="display:none;";
+    document.querySelector(".videos").style.cssText="display:none;";
+    document.querySelector(".photo").style.cssText="display:block;";
+    document.querySelector(".more").style.cssText="display:none;";
+}
+function show4(){
+    document.querySelector(".home").style.cssText="display:none;";
+    document.querySelector(".about").style.cssText="display:none;";
+    document.querySelector(".videos").style.cssText="display:none;";
+    document.querySelector(".photo").style.cssText="display:none;";
+    document.querySelector(".more").style.cssText="display:block;";
+}
